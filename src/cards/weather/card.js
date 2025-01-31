933d0f83caef110e874687561771708e174bfbbd
adrien-parasote <adrien.parasote@gmail.com>
2025-01-31 13:59:58 +0100

    format

--- src/cards/weather/card.js
+++ src/cards/weather/card.js
@@ -2,8 +2,8 @@ import Chart from 'chart.js/auto';
 import {LitElement, html, nothing} from 'lit';
 import {isEqual} from 'lodash-es';
 
-import '../../helpers/components/tiles.js';
 import common_style from '../../helpers/common_style.js';
+import '../../helpers/components/tiles.js';
 import {
   DailyForecast,
   HourlyForecast,

8263eeb8ff531634831063a80bd9698d1d5d5e6b
adrien-parasote <adrien.parasote@gmail.com>
2025-03-07 15:31:49 +0100

    Add vehicle car metadata

--- src/cards/vehicles/config-metadata.js
+++ src/cards/vehicles/config-metadata.js
@@ -1,1 +1,99 @@
-export default {};
+export default {
+  vehicles: {
+    mandatory: false,
+    type: 'array',
+    default: [],
+    data_type: 'object',
+    data: {
+      name: {
+        mandatory: true,
+        type: 'string',
+      },
+      charging: {
+        mandatory: true,
+        type: 'string',
+      },
+      rear_right_door_status: {
+        mandatory: false,
+        type: 'string',
+        default: '',
+      },
+      rear_left_door_status: {
+        mandatory: true,
+        type: 'string',
+      },
+      driver_door_status: {
+        mandatory: true,
+        type: 'string',
+      },
+      passenger_door_status: {
+        mandatory: true,
+        type: 'string',
+      },
+      plugged_in: {
+        mandatory: true,
+        type: 'string',
+      },
+      lock_status: {
+        mandatory: true,
+        type: 'string',
+      },
+      hatch_status: {
+        mandatory: true,
+        type: 'string',
+      },
+      location: {
+        mandatory: true,
+        type: 'string',
+      },
+      battery_autonomy: {
+        mandatory: true,
+        type: 'string',
+      },
+      fuel_autonomy: {
+        mandatory: true,
+        type: 'string',
+      },
+      battery_level: {
+        mandatory: true,
+        type: 'string',
+      },
+      battery_last_activity: {
+        mandatory: true,
+        type: 'string',
+      },
+      location_last_activity: {
+        mandatory: true,
+        type: 'string',
+      },
+      battery_available_energy: {
+        mandatory: true,
+        type: 'string',
+      },
+      charge_state: {
+        mandatory: true,
+        type: 'string',
+      },
+      plug_state: {
+        mandatory: true,
+        type: 'string',
+      },
+      mileage: {
+        mandatory: true,
+        type: 'string',
+      },
+      fuel_quantity: {
+        mandatory: true,
+        type: 'string',
+      },
+      battery_temperature: {
+        mandatory: true,
+        type: 'string',
+      },
+      charging_remaining_time: {
+        mandatory: true,
+        type: 'string',
+      },
+    },
+  },
+};
